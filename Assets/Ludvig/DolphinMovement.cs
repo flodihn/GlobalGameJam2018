a5bb5f82c6745769eda36239e2be97b67db3f3e5
Ludvig Jare <Ludvig.jare@hotmail.se>
2018-01-26 20:35:20 +0100

    CameraFollow And SimpleMOvement

--- Assets/Ludvig/DolphinMovement.cs
+++ Assets/Ludvig/DolphinMovement.cs
@@ -2,15 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class DolphinMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Rigidbody2D rigidbody;
+
+    public float speed = 10;
+
+    public float rotateSpeed = 5;
+
+    public bool mouseMovement;
+
+    private void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        KeyBoardMovement();
+
+
+
+        if(mouseMovement == true) {
+            MouseMovement();
+        }
+    }
+
+    void KeyBoardMovement() {
+        float horizontal = -Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Rotate(0,0, horizontal * rotateSpeed);
+
+        rigidbody.AddForce(transform.up * speed);
+        if (horizontal > 0 || horizontal < 0) {
+            rigidbody.AddForce(-transform.up * speed);
+        }
+    }
+
+    void MouseMovement() {
+        Vector3 forceDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        transform.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        print(forceDir);
+        Debug.DrawRay(transform.position, forceDir);
+
+        rigidbody.AddForce(forceDir * speed);
+    }
 }
