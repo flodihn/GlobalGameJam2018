7ca58a5c281a902c27ad05084d68f93ccb635182
cflow <christian@flodihn.se>
2018-01-26 22:36:10 +0100

    Working on radio waves.

---
+++ Assets/Christian/Scripts/RadioWaveCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioWaveCollider : MonoBehaviour {
+
+	private List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent> ();
+	public ParticleSystem myParticleSystem;
+
+	void Start() {
+		myParticleSystem = GetComponent<ParticleSystem> ();
+	}
+
+	public void OnParticleCollision(GameObject other) {
+		ParticlePhysicsExtensions.GetCollisionEvents (myParticleSystem, other, collisionEvents);
+		for (int i = 0; i < collisionEvents.Count; i++) {
+			Debug.Log ("Particle collided with " + other.name);
+		}
+	}
+}
