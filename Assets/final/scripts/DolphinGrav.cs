2b85ddd21d32dca1d56a6138a91ed35a110845ea
cflow <christian@flodihn.se>
2018-01-27 21:35:07 +0100

    Added dolphin gravitation and fuel.

---
+++ Assets/final/scripts/DolphinGrav.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DolphinGrav : MonoBehaviour {
+	public float power;
+	Rigidbody2D myRigidBody;
+
+	void Start() {
+		myRigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate() {
+		float gravMultiplier = transform.position.y;
+
+		myRigidBody.AddForce (new Vector3 (0, -gravMultiplier * power, 0));
+	}
+}
