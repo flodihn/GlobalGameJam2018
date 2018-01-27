60fb46c4ec4c7a83c73732c2367a90f0dcbdeead
Mikael <mikael.willard@gmail.com>
2018-01-27 20:29:21 +0100

    wohoooo

---
+++ Assets/final/scripts/nukeTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nukeTrigger : MonoBehaviour {
+
+	public Slider kimSlider;
+	public GameObject nukePrefab;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void nukeCHange() {
+		print ("works has value " + kimSlider.value);
+		if (kimSlider.value == kimSlider.maxValue) {
+			GameObject.Instantiate (nukePrefab, Vector3.zero, Quaternion.identity);
+		}
+
+	}
+
+
+}
