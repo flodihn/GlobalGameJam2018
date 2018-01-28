9c5fe5bf64b6f5b4440a693ad455484094de0699
Mikael <mikael.willard@gmail.com>
2018-01-28 11:50:25 +0100

    Made changes to final scene and added story text

---
+++ Assets/final/scripts/StoryStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryStart : MonoBehaviour {
+
+	public int sceneNumber;
+
+
+	public void loadScene() {
+		SceneManager.LoadScene (sceneNumber);
+	}
+}
